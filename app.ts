f74765a809ba7c851e2ad2f229bf3e1572b3d1cb
Abhishek kumar <abhi.arya97161@gmail.com>
2024-02-07 19:33:48 +0530

    Integrating Typescript with Node and Express

--- app.ts
+++ app.ts
@@ -1,47 +1,12 @@
-const num1Element = document.getElementById("num1") as HTMLInputElement;
-const num2Element = document.getElementById("num2") as HTMLInputElement;
-const buttonElement = document.querySelector('button')!;
+import express from "express";
+import bodyParser from "body-parser";
 
-const numResults: Array<number> = [];
-const textResults: string[] = [];
+import todosRoutes from "./routes/todos";
 
-type NumOrString = Number | string;
-type Result = { val: number; timestamp: Date };
+const app = express();
 
-interface ResultObj {
-    val: number;
-    timestamp: Date
-}
+app.use(bodyParser.json())
 
-function add(num1: NumOrString, num2: NumOrString) {
-    if (typeof num1 === 'number' && typeof num2 === 'number') {
-        return num1 + num2;
-    } else if (typeof num1 === 'string' && typeof num2 === 'string') {
-        return num1 + ' ' + num2;
-    }
-    return +num1 + +num2;
-}
+app.use(todosRoutes);
 
-function printResult(resultObj: ResultObj) {
-    console.log(resultObj.val)
-}
-buttonElement.addEventListener('click', () => {
-    const num1 = num1Element.value;
-    const num2 = num2Element.value;
-    const result = add(+num1, +num2);
-    numResults.push(result as number)
-    const stringResult = add(num1, num2)
-    textResults.push(stringResult as string)
-    printResult({ val: result as number, timestamp: new Date() })
-    console.log(numResults, textResults)
-})
-
-const myPromise = new Promise<string>((resolve, reject) => {
-    setTimeout(() => {
-        resolve('It Worked')
-    }, 1000)
-})
-
-myPromise.then((result) => {
-    console.log(result.split('W')); 
-})
+app.listen(3000)
