1545f0ef6828b5f51cbcdf3b3fdafdfed27bf5b6
Abhishek kumar <abhi.arya97161@gmail.com>
2024-02-08 14:29:36 +0530

    Typescript showdown

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -7,27 +7,31 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     res.status(201).json({ message: 'Added Todo', todo: newTodo, todos: todos });
 });
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params;
+    const tid = params.todoId;
+    const body = req.body;
     const todoIndex = todos.findIndex((todoItem) => {
         return todoItem.id === tid;
     });
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
         return res.status(200).json({ message: 'Updated todo', todos: todos });
     }
     res.status(404).json({ message: 'Could not find' });
 });
 router.delete('/todo/:todoId', (req, res, next) => {
+    const params = req.params;
     todos = todos.filter((todosItem) => {
-        todosItem.id !== req.params.todoId;
+        todosItem.id !== params.todoId;
         res.status(200).json({ message: "Deleted", todos: todos });
     });
 });
